5e51f6fbd0ffafa6cdecbdf93a3c971b98513bc0
ekate <ekatereanagricaenko@gmail.com>
2019-06-10 16:54:08 +0300

    rewrite methods in (StringUtils)

--- src/test/java/labs/introtoprogramming/lab5/object/Builder/FacePlateTests.java
+++ src/test/java/labs/introtoprogramming/lab5/object/Builder/FacePlateTests.java
@@ -4,14 +4,18 @@ import labs.introtoprogramming.lab5.object.Builder.POJO.FacePlate;
 import labs.introtoprogramming.lab5.object.Builder.POJO.VertexFace;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class FacePlateTests {
 
 
+    @Test
     public void testCalculateNormal(){
 
         VertexFace first = new VertexFace();
         VertexFace second = new VertexFace();
         VertexFace third = new VertexFace();
+        assertEquals(first, second);
     }
 
 }
