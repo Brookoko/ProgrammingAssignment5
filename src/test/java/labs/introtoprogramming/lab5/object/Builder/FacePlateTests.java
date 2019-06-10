992b2057ac60117767e3aad150f5334c61a5348b
ekate <ekatereanagricaenko@gmail.com>
2019-06-10 17:01:47 +0300

    fix problems with test

--- src/test/java/labs/introtoprogramming/lab5/object/Builder/FacePlateTests.java
+++ src/test/java/labs/introtoprogramming/lab5/object/Builder/FacePlateTests.java
@@ -1,7 +1,7 @@
 package labs.introtoprogramming.lab5.object.Builder;
 
 import labs.introtoprogramming.lab5.object.Builder.POJO.FacePlate;
-import labs.introtoprogramming.lab5.object.Builder.POJO.VertexFace;
+import labs.introtoprogramming.lab5.object.Builder.POJO.Material;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,12 +10,15 @@ public class FacePlateTests {
 
 
     @Test
-    public void testCalculateNormal(){
 
-        VertexFace first = new VertexFace();
-        VertexFace second = new VertexFace();
-        VertexFace third = new VertexFace();
-        assertEquals(first, second);
+    public void testFacePlate() {
+        String materStr = "firstMaterial";
+        String mapStr = "map";
+        Material material = new Material(materStr);
+        Material map = new Material(mapStr);
+        FacePlate fp = new FacePlate(material, map);
+        assertEquals(mapStr, fp.getMap());
+        assertEquals(materStr, fp.getMaterial());
     }
 
 }
